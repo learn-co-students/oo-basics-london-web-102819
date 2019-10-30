a63907386fc0cb55048d9ec7c9bc70d072e4b531
PianoFF <Fangfei1star@gmail.com>
2019-10-30 23:21:03 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,35 @@
+class Book
+    attr_accessor :author, :page_count, :genre
+    attr_reader :title 
+    def initialize (title)
+        @title = title
+    end
+    # def title   ==> only need to read? a reader
+    #     @title
+    # end
+    # def author=(author)
+    #     @author = author
+    # end
+    # def author
+    #     @author
+    # end
+    # def page_count=(page)
+    #     @page_count = page
+    # end
+    # def page_count
+    #     @page_count
+    # end
+    # def genre=(genre)
+    #     @genre = genre 
+    # end
+    # def genre
+    #     @genre
+    # end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end 
+
+    
+end
 
 
